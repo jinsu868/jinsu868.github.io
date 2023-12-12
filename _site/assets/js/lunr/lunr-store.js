ef5813d1f38a8a9d5a08397b064652289fc8e97c
jinsu868 <mjinsu098@gmail.com>
2023-12-12 22:04:36 +0900

    docs: CS-스프링

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,38 +1,38 @@
 var store = [{
-        "title": "[Spring] 객체 지향 설계와 스프링",
-        "excerpt":"객체 지향 설계와 스프링 스프링의 핵심 좋은 객체 지향 application을 만들 수 있게 도와주는 프레임워크 SOLID SRP (Single responsiblity principle) 한 클래스는 하나의 책임만 가져야 한다. 변경파급이 낮게 설계하는 것이 좋다. OCP (Open Closed Principle) 확장에는 열려 있고 변경에는 닫혀 있게 설계해야 한다. 기능을 추가하거나 변경할 때, 클라이언트 코드의 변경...","categories": ["Spring"],
+        "title": "[Spring] 객체지향 프로그래밍",
+        "excerpt":"객체지향 프로그래밍 정의 Object-oriented programming (OOP) is a computer programming model that organizes software design around data, or objects, rather than functions and logic. An object can be defined as a data field that has unique attributes and behavior. 객체지향 프로그래밍은 기능과 로직보다는 데이터, 객체를 중심으로 소프트웨어를 설계하는 컴퓨터 프로그래밍...","categories": ["Spring"],
         "tags": ["Spring"],
-        "url": "/spring/Spring1/",
+        "url": "/spring/1/",
         "teaser": null
       },{
-        "title": "[Spring] 스프링 핵심 원리 이해",
-        "excerpt":"본 포스트는 Inflearn의 김영한님 강의를 바탕으로 작성했습니다. 스프링의 핵심 원리 아래 코드의 문제점은 무엇일까? public class OrderServiceImpl implements OrderService { private final MemberRepository memberRepository = new MemoryMemberRepository(); private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); } 클라이언트인 OrderServiceImpl이 구현체를 직접 의존하고 있다. (DIP 위반) 만약 저장소를 DB로 바꾸려고 하거나, 할인 정책을...","categories": ["Spring"],
+        "title": "[Spring] IoC, DI 컨테이너",
+        "excerpt":"IoC (Inversion of Control) 정의 IoC란 객체의 생성, 생명주기의 관리까지 모든 객체에 대한 제어권이 바뀌었다는 것을 의미한다. 위 정의만 읽고 IoC가 무엇인지 와닿지 않을 수 있다. 아래 예시를 보고 이해해보자. 우리는 기존에 코드를 작성할 때, 각 객체에서 자신이 필요한 객체를 생성하고 실행하도록 했었다. ManagerController 클래스가 파일 입출력에 관련된 객체(FileInputView, FileOutputVIew)를...","categories": ["Spring"],
         "tags": ["Spring"],
-        "url": "/spring/Spring2/",
+        "url": "/spring/2/",
         "teaser": null
       },{
-        "title": "[Spring] 스프링 컨테이너와 스프링 빈",
-        "excerpt":"본 포스트는 Inflearn의 김영한님 강의를 바탕으로 작성했습니다. 스프링 컨테이너 생성 과정 스프링 컨테이너 생성 new AnnotationConfigApplicationContext(AppConfig.class) 스프링 컨테이너를 생성할 때는 구성 정보를 지정해줘야 한다. (AppConfig) 스프링 빈 등록 설정 정보 클래스 (AppConfig)를 통해서 스프링 빈을 등록한다. 빈 이름은 default로 메서드 이름이 사용된다. (직접 지정할 수도 있다.) 빈 이름은 항상 다른...","categories": ["Spring"],
+        "title": "[Spring] 스프링 컨테이너",
+        "excerpt":"스프링 컨테이너 스프링 컨테이너란? 스프링 컨테이너는 IoC 컨테이너이다. IoC 컨테이너에 대해 잘 모른다면 이전 포스트를 참고하자. 스프링 컨테이너를 지칭할 때, ApplicationContext, BeanFactory로 나누는데 일반적으로 BeanFactory를 직접 사용할 일이 없기 때문에 ApplicationContext를 스프링 컨테이너라고 생각하면 된다. 생성 과정 스프링 컨테이너 생성 스프링 빈들을 저장할 컨테이너를 먼저 생성한다. 빈은 이름과 실제 객체를...","categories": ["Spring"],
         "tags": ["Spring"],
-        "url": "/spring/Spring3/",
+        "url": "/spring/3/",
         "teaser": null
       },{
         "title": "[Spring] 싱글톤 컨테이너",
         "excerpt":"본 포스트는 Inflearn의 김영한님 강의를 바탕으로 작성했습니다. 싱글톤 패턴을 사용하는 이유 여러 클라이언트가 요청할 때마다 새로운 객체를 생성하는 것은 메모리 낭비가 심하다. 객체를 하나만 생성하고 이를 공유하도록 설계하자. -&gt; 싱글톤 패턴 싱글톤 패턴 클래스의 인스턴스를 딱 1개만 생성되도록 보장하는 디자인 패턴이다. public class SingletonService { private static final SingletonService instance...","categories": ["Spring"],
         "tags": ["Spring"],
-        "url": "/spring/Spring4/",
+        "url": "/spring/4/",
         "teaser": null
       },{
         "title": "[Spring] 컴포넌트 스캔",
         "excerpt":"본 포스트는 Inflearn의 김영한님 강의를 바탕으로 작성했습니다. 컴포넌트 스캔 스프링은 설정 정보가 없어도 자동으로 스프링 빈을 등록하는 컴포넌트 스캔이라는 기능을 제공한다. 컴포넌트 스캔을 사용하면 @Component가 붙은 클래스를 찾아서 다 빈으로 등록한다. @Repository, @Service, @Configuration, @Controller를 까보면 안에 @Component를 가지고 있다. -&gt; 다 빈으로 등록된다.(Spring이 제공하는 기능) 컴포넌트 스캔을 사용하려면 @ComponentScan을...","categories": ["Spring"],
         "tags": ["Spring"],
-        "url": "/spring/Spring5/",
+        "url": "/spring/5/",
         "teaser": null
       },{
         "title": "[Spring] 의존관계 자동 주입",
         "excerpt":"본 포스트는 Inflearn의 김영한님 강의를 바탕으로 작성했습니다. 의존관계 주입 방식 생성자 주입 @Component public class OrderServiceImpl implements OrderService { private final MemberRepository memberRepository; private final DiscountPolicy discountPolicy; @Autowired public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { this.memberRepository = memberRepository; this.discountPolicy = discountPolicy; } } 생성자를 통해서 의존관계를 주입 받는 방식이다. 생성자 호출시점에...","categories": ["Spring"],
         "tags": ["Spring"],
-        "url": "/spring/Spring6/",
+        "url": "/spring/6/",
         "teaser": null
       },{
         "title": "[Spring] 빈 생명주기 콜백",
